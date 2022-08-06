5e89726c157244940cc5ef23ca7523259061d185
Marios Pantzaras <marios.pantzaras@anelixis.com>
2022-08-06 15:09:49 +0300

    Add header

--- src/modules/_app/app.component.ts
+++ src/modules/_app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 
 
@@ -11,16 +14,23 @@ import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
+  private currentUrlSubj: BehaviorSubject<string> = new BehaviorSubject(null);
+  public currentUrlObs: Observable<string> = this.currentUrlSubj.asObservable();
 
 
 
   constructor(
-
+    private router: Router
   ) {}
 
 
 
   public ngOnInit() {
+
+    // observe for url changes
+    this.router.events
+    .pipe(filter((event) => event instanceof NavigationEnd))
+    .subscribe((event: NavigationEnd) => this.currentUrlSubj.next(event.urlAfterRedirects));
 
   }
 
